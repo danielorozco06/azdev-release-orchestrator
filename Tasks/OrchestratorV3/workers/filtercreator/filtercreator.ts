ba0f4be3e0c0d2e9209e984f7b9423741062cdb6
daorozc <daorozc@bancolombia.com.co>
2024-03-12 14:40:24 -0500

    Add filter creation methods to FilterCreator

--- Tasks/OrchestratorV3/workers/filtercreator/filtercreator.ts
+++ Tasks/OrchestratorV3/workers/filtercreator/filtercreator.ts
@@ -9,16 +9,38 @@ import { IFilters } from "../../helpers/taskhelper/ifilters";
 import { IRepositoryFilter } from "./irepositoryfilter";
 import { IPipelineFilter } from "./ipipelinefilter";
 
+/**
+ * FilterCreator class is responsible for creating filters based on the provided IFilters.
+ */
 export class FilterCreator implements IFilterCreator {
 
     private debugLogger: IDebug;
 
+    /**
+     * Constructs a new instance of FilterCreator with a logger.
+     * @param {ILogger} logger - The logger to use for debugging.
+     */
     constructor(logger: ILogger) {
 
         this.debugLogger = logger.extend(this.constructor.name);
 
     }
 
+    /**
+     * Creates a resources filter based on the given filters.
+     * @param {IFilters} filters - The filters to use for creating the resources filter.
+     * @returns {Promise<IResourcesFilter>} A promise that resolves to an IResourcesFilter object.
+     * @throws {Error} Throws an error if there is an issue during the creation of the resources filter.
+     *
+     * ### Steps:
+     * 1. Extend the debug logger with the name of the current method.
+     * 2. Initialize an empty resources filter object.
+     * 3. If branchName is present in filters, add a repository filter for 'self'.
+     * 4. Iterate over pipelineResources in filters and add corresponding pipeline filters.
+     * 5. Iterate over repositoryResources in filters and add corresponding repository filters.
+     * 6. Log the created resources filter.
+     * 7. Return the resources filter.
+     */
     public async createResourcesFilter(filters: IFilters): Promise<IResourcesFilter> {
 
         const debug = this.debugLogger.extend(this.createResourcesFilter.name);
@@ -82,6 +104,21 @@ export class FilterCreator implements IFilterCreator {
 
     }
 
+    /**
+     * Creates a build filter based on the given filters.
+     * @param {IFilters} filters - The filters to use for creating the build filter.
+     * @returns {Promise<IBuildFilter>} A promise that resolves to an IBuildFilter object.
+     * @throws {Error} Throws an error if there is an issue during the creation of the build filter.
+     *
+     * ### Steps:
+     * 1. Extend the debug logger with the name of the current method.
+     * 2. Initialize an empty build filter object with default statuses.
+     * 3. Set the buildResult in the build filter if it's specified in filters.
+     * 4. Set the tagFilters in the build filter based on filters.
+     * 5. Set the branchName in the build filter if it's specified in filters.
+     * 6. Log the created build filter.
+     * 7. Return the build filter.
+     */
     public async createBuildFilter(filters: IFilters): Promise<IBuildFilter> {
 
         const debug = this.debugLogger.extend(this.createBuildFilter.name);
--- Tasks/OrchestratorV3/workers/filtercreator/ifiltercreator.ts
+++ Tasks/OrchestratorV3/workers/filtercreator/ifiltercreator.ts
@@ -2,9 +2,24 @@ import { IFilters } from "../../helpers/taskhelper/ifilters";
 import { IBuildFilter } from "./ibuildfilter";
 import { IResourcesFilter } from "./iresourcesfilter";
 
+/**
+ * FilterCreator class is responsible for creating filters based on the provided IFilters.
+ */
 export interface IFilterCreator {
-
+    /**
+     * Creates a resources filter based on the given filters.
+     * @param {IFilters} filters - The filters to use for creating the resources filter.
+     * @returns {Promise<IResourcesFilter>} A promise that resolves to an IResourcesFilter object.
+     * @throws {Error} Throws an error if there is an issue during the creation of the resources filter.
+     */
     createResourcesFilter(filters: IFilters): Promise<IResourcesFilter>;
+
+    /**
+     * Creates a build filter based on the given filters.
+     * @param {IFilters} filters - The filters to use for creating the build filter.
+     * @returns {Promise<IBuildFilter>} A promise that resolves to an IBuildFilter object.
+     * @throws {Error} Throws an error if there is an issue during the creation of the build filter.
+     */
     createBuildFilter(filters: IFilters): Promise<IBuildFilter>;
 
 }
